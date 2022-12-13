01bc235919d811b3568d9acaa2d7c38a0264343a
Dominik V <dudydomy@gmail.com>
2022-12-13 19:05:57 +0200

    V1.1

--- src/MyClass.java
+++ src/MyClass.java
@@ -3,6 +3,10 @@ public class MyClass {
         System.out.println("git test");
         System.out.println("funct 2 ");
         System.out.println("ABC");
+        int a=10,b=100;
+        int c = a + b;
+        System.out.println(c);
+
 
 
     }
